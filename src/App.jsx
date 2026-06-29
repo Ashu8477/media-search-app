752cca6dbddfa30f36afd3bee8a86a09f735c2d7
Ashu Singh <singhaashu271@gmail.com>
2026-06-29 23:18:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Navbar from './components/Navbar';
+import CollectionPage from './pages/CollectionPage';
+import HomePage from './pages/HomePage';
+
+const App = () => {
+  return (
+    <div className="min-h-screen text-white w-full bg-gray-950">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/collection" element={<CollectionPage />} />
+      </Routes>
+
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default App;
